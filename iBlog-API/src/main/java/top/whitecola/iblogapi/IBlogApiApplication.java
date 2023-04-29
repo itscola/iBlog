2175fbcbc72e6cb6fde424f22d90b7fe4019700f
white_cola <white_cola@foxmail.com>
2023-04-29 16:07:17 +0800

    init

---
+++ iBlog-API/src/main/java/top/whitecola/iblogapi/IBlogApiApplication.java
@@ -0,0 +1,13 @@
+package top.whitecola.iblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IBlogApiApplication.class, args);
+    }
+
+}
---
+++ iBlog-API/src/test/java/top/whitecola/iblogapi/IBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.whitecola.iblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
